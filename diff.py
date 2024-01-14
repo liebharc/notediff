bcbc3aaf9c238e3ef514d4c4d62a7aa7d55f17c5
Christian Liebhardt <christian.liebhardt@arcor.de>
2024-01-14 21:33:54 +0100

    First diff version

--- diff.py
+++ diff.py
@@ -9,10 +9,13 @@ def norm_string(s: str):
     s = re.sub(r'\d+', '', s).strip()
     return s.split(" ")
 
+def diff(a, b):
+    return difflib.SequenceMatcher(None, a, b).get_matching_blocks()
+
 for line in file_content:
     parts = line.strip().split(",")
     reference = parts[0]
     actual = parts[1]
     reference = norm_string(reference)
     actual = norm_string(actual)
-    print(reference, "vs", actual)
+    print(str.join("", reference), "vs", str.join("", actual), diff(reference, actual))
